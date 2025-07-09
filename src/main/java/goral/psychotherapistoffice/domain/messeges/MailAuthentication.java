e1b8a15fce961bdf46535b9b1283bea2f911d9ce
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-07-09 23:06:56 +0200

    Use configuration provider for email authentication credentials.

--- src/main/java/goral/psychotherapistoffice/domain/messeges/MailAuthentication.java
+++ src/main/java/goral/psychotherapistoffice/domain/messeges/MailAuthentication.java
@@ -1,12 +1,17 @@
 package goral.psychotherapistoffice.domain.messeges;
 
+import goral.psychotherapistoffice.config.ConfigProvider;
 import jakarta.mail.Authenticator;
 import jakarta.mail.PasswordAuthentication;
 
 public class MailAuthentication extends Authenticator {
 
+
+    String email = ConfigProvider.get("gmailcredentials.email");
+    String password = ConfigProvider.get("gmailcredentialscredentials.password");
+
     @Override
     protected PasswordAuthentication getPasswordAuthentication() {
-        return new  PasswordAuthentication("gabinet.ewa.gorska@gmail.com", "example");
+        return new  PasswordAuthentication(email, password);
     }
 }
